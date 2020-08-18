3a4bcfa5aed510530d2a3d8b6a1a57d75e2c23c6
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-18 05:04:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,99 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #arraychar = self.split("")
+    #arraychar.last == "." ? true : false
+    self.buddy
+    self.end_with?(".") ? true : false
   end
 
   def question?
+    self.end_with?("?") ? true : false
 
+  end
+
+  def buddy
+    puts "hello buddy"
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
+
+  def clearmulti(string, piecetoremove)
+    lateststring = ""
+    cuts = 0
+
+    loop do
+      string.slice! piecetoremove
+      if string == lateststring
+        #no need to add cuts here, if string == lateststring, then the last slice was useless
+        break
+      else
+        cuts += 1
+        lateststring = string.dup #making a value copy
+      end
+    end #end loop
+
+    return [string, cuts]
+  end
+
 
   def count_sentences
+    #find all the !!, !!!, ..., .., ???, ??, counteach as one punctuation, remove it,
+    #WARNING: ORDER MATTERS, first count the !!!, remove them, then count the !!, reverse order is messedup
 
+     #gotta design a logic chain to tally up how many times u've removed things
+     #about clearmulti: accepts string and piecetoremove, returns an array of [string, cuts]. final string and # of real cuts made
+     tally = 0
+     copyofstring = self.dup
+
+     marks = ["???", "??", "?", "...", "..", ".", "!!!", "!!", "!"]
+     marks.each do | mark |
+       #nots ure what the slef would refer to here. it's called inside an iterated array. dont want to self to be the array
+       #result = self.clearmulti(copyofstring, mark)
+       #copyofstring = result[0]
+       #tally += result[1]
+       self.buddy
+     end
+
+     tally
   end
+
+
+
 end
+
+hi = "beyonce."
+puts hi.sentence?
+
+def clearmulti(string, piecetoremove)
+  lateststring = ""
+  cuts = 0
+  loop do
+
+    string.slice! piecetoremove
+    binding.pry
+    if string == lateststring
+      binding.pry
+      #no need to add cuts here, if string == lateststring, then the last slice was useless
+      break
+    else
+      binding.pry
+      cuts += 1
+      lateststring = string.dup #making a value copy
+      binding.pry
+    end
+  end #end loop
+  return [string, cuts]
+end
+puts "landmark"
+#puts thisstuff = clearmulti("hello???hello???beyonce???cardib???hi?", "???")
+#puts "how many cuts: #{thisstuff[1]}"
+#it's counting but its MISCOUNTING by one extra. !!!!!!!
+
+something = "whatever is good! is also bad!!! somewhere else... i think."
+something.count_sentences
+
+
+#puts otherstuff = clearmulti(thisstuff[0], "?")
